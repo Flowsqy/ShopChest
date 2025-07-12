785b7438811d449a8349feb3146ed15876e38707
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-07-12 20:17:32 +0200

    Add a way to publish nms interface module to maven local so it can be used by spigot nms submodules

--- nms/interface/build.gradle.kts
+++ nms/interface/build.gradle.kts
@@ -1,3 +1,17 @@
 plugins {
     id("buildlogic.java-application-conventions")
+    `maven-publish`
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "de.epiceric"
+            artifactId = "shopchest-nms-interface"
+            version = "1.0.0"
+
+            from(components["java"])
+        }
+    }
+}
+
